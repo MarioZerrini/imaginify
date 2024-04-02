cea9e38c96e95bf6f54429fb373d2a561ffeb8e2
Mario Zerrini <mario.zerrini.2001@gmail.com>
2024-04-02 12:08:46 +0300

    final version

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/", "/api/webhooks/clerk", "/api/webhooks/stripe"],
+  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)", "/transformations/660a695f34c5f9f05f982f97"]
 });
 
 export const config = {
