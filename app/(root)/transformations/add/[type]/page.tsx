e2aad2451b7a3fa0b06ecb64735ee460edd3f7a2
Mario Zerrini <mario.zerrini.2001@gmail.com>
2024-04-02 11:08:17 +0300

    final version

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,6 +1,32 @@
-const AddTransformationTypePage = () => {
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
+  const transformation = transformationTypes[type];
+
+  if (!userId) redirect("sign-in");
+
+  // we need the id of the user in the database not that of clerk
+  const user = await getUserById(userId);
   return (
-    <div>AddTransformationTypePage</div>
-  )
-}
-export default AddTransformationTypePage
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
+  );
+};
+export default AddTransformationTypePage;
--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -1,10 +1,10 @@
-import { model, Schema, models } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
 
 export interface IImage extends Document {
   title: string;
   transformationType: string;
   publicId: string;
-  secureUrl: string;
+  secureURL: string;
   width?: number;
   height?: number;
   config?: object;
@@ -12,33 +12,32 @@ export interface IImage extends Document {
   aspectRatio?: string;
   color?: string;
   prompt?: string;
-  author?: {
+  author: {
     _id: string;
     firstName: string;
     lastName: string;
-  }; // Assuming author is represented by user's ID
-  createdAt: Date;
-  updatedAt: Date;
+  };
+  createdAt?: Date;
+  updatedAt?: Date;
 }
-
 
 const ImageSchema = new Schema({
   title: { type: String, required: true },
   transformationType: { type: String, required: true },
   publicId: { type: String, required: true },
-  secureUrl: { type: URL, required: true },
+  secureURL: { type: String, required: true },
   width: { type: Number },
   height: { type: Number },
   config: { type: Object },
-  transformationUrl: { type: URL },
+  transformationUrl: { type: String },
   aspectRatio: { type: String },
   color: { type: String },
   prompt: { type: String },
-  author: {type: Schema.Types.ObjectId, ref: 'User'},
-  createdAt: { type: Date, default: Date.now},
-  updatedAt: { type: Date, default: Date.now},
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
-const Image = models?.Image || model('Image', ImageSchema);
+const Image = models?.Image || model("Image", ImageSchema);
 
 export default Image;
